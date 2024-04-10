4cb89e937d4c9b1b489a74381a4c37c21d223a50
Joel Lehman <joel@MacBook-Pro-8.local>
2024-04-10 13:19:20 -0700

    updated meta with new toks

--- data/bookgpt/meta_writer.py
+++ data/bookgpt/meta_writer.py
@@ -3,6 +3,8 @@ import pickle
 
 vocab_size = 23916
 vocab_size = 23913
+vocab_size = 23925
+vocab_size = 23936 #nearest multiple of 64
 out_dict = {'vocab_size':vocab_size}
 
 #a = np.fromfile("val.bin")
